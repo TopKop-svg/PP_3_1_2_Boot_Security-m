21746f63737228de30a724359f1d27e86925d274
TopKop-svg <loookintome@gmail.com>
2024-04-19 05:00:30 +0900

    (mod) clean comments

--- src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -5,19 +5,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 import ru.kata.spring.boot_security.demo.models.Role;
 import ru.kata.spring.boot_security.demo.models.User;
-import ru.kata.spring.boot_security.demo.repositories.RoleRepository;
 import ru.kata.spring.boot_security.demo.services.UserService;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -25,7 +20,6 @@ import java.util.Set;
 
 
 @Controller
-//@RequestMapping("/admin")
 public class AdminController {
     private final UserService userService;
 
@@ -75,7 +69,6 @@ public class AdminController {
         }
         logger.info("Saving new user: {}", newUser);
 
-       // System.out.println("usernameMy: " + newUser.getUsername());
         userService.saveUser(newUser);
         return "redirect:/admin";
     }
@@ -101,57 +94,5 @@ public class AdminController {
         return "redirect:/admin";
     }
 
-   /* @GetMapping
-    public String index(Model model) {
-        model.addAttribute("users", userService.getAllUsers());
-        return "admin-panel";
-    }
-
-    @GetMapping("/new")
-    public String newUser(Model model) {
-        model.addAttribute("user", new User());
-        model.addAttribute("allRoles", userService.getAllRoles());
-        return "new";
-    }
-
-    @PostMapping
-    public String create(@ModelAttribute("user") User user) {
-        userService.saveUser(user);
-        return "redirect:/admin";
-    }
-
-    @GetMapping("/{id}")
-    public String show(@PathVariable("id") int id, Model model) {
-        model.addAttribute("userId", userService.showUserById(id));
-        return "id";
-    }
-
-    @GetMapping("/{id}/edit")
-    public String edit(Model model, @PathVariable("id") int id) {
-        model.addAttribute("user", userService.showUserById(id));
-        model.addAttribute("allRoles", userService.getAllRoles());
-        return "edit";
-    }
-
-    @PostMapping("/{id}/update")
-    public String update(@ModelAttribute("user") User user, @PathVariable("id") int id) {
-        userService.updateUserById(id, user);
-        return "redirect:/admin";
-    }
-
-    @PostMapping("/{id}/delete")
-    public String delete(@PathVariable("id") int id) {
-        userService.deleteUserById(id);
-        return "redirect:/admin";
-    }
-
-    @GetMapping("/logout")
-    public String logout(HttpServletRequest request) {
-        HttpSession session = request.getSession(false);
-        if (session != null) {
-            session.invalidate();
-        }
-        return "redirect:/login";
-    }*/
 
 }
--- src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
@@ -7,14 +7,11 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import ru.kata.spring.boot_security.demo.models.User;
 import ru.kata.spring.boot_security.demo.services.UserService;
 
-import java.security.Principal;
 
 @Controller
-//@RequestMapping("/user")
 public class UserController {
 
     private final UserService userService;
@@ -31,19 +28,6 @@ public class UserController {
         return "user_panel";
     }
 
-    /*@GetMapping
-    public String showUser(Principal principal, Model model) {
-        User user = userService.findByUsername(principal.getName());
-        model.addAttribute("id", user.getId());
-        model.addAttribute("username", user.getUsername());
-        model.addAttribute("email", user.getEmail());
-        model.addAttribute("password", user.getPassword());
-        model.addAttribute("name", user.getName());
-        model.addAttribute("age", user.getAge());
-        model.addAttribute("roles", user.getRoles());
-
-        return "user/user_panel";
-    }*/
 
 
 }
