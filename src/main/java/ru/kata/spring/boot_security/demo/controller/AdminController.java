f8ed899ff1088a48ca49723e236342128c35640f
TopKop-svg <loookintome@gmail.com>
2024-04-26 00:14:51 +0900

    fix AdminController.java index and save method

--- src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -1,7 +1,5 @@
 package ru.kata.spring.boot_security.demo.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -9,28 +7,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
-import ru.kata.spring.boot_security.demo.models.Role;
 import ru.kata.spring.boot_security.demo.models.User;
-import ru.kata.spring.boot_security.demo.services.RoleService;
-import ru.kata.spring.boot_security.demo.services.RoleServiceImpl;
 import ru.kata.spring.boot_security.demo.services.UserService;
-
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 
 
 @Controller
 public class AdminController {
     private final UserService userService;
-    private final RoleService roleService;
 
     @Autowired
-    public AdminController(UserService userService, RoleService roleService) {
+    public AdminController(UserService userService) {
         this.userService = userService;
-        this.roleService = roleService;
 
     }
 
@@ -52,36 +40,33 @@ public class AdminController {
 
     @GetMapping(value = "/admin")
     public String index(ModelMap model, @AuthenticationPrincipal UserDetails authenticatedUser) {
-        //User currentUser = userService.findByUsername(authenticatedUser.getUsername());
         model.addAttribute("listUsers", userService.getAllUsers());
-        /* model.addAttribute("listRoles", roleService.getAllRoles());*/
         model.addAttribute("user",  userService.findByUsername(authenticatedUser.getUsername()));
-        //model.addAttribute("newUser", new User());
-        //model.addAttribute("listRoles", roleService.getAllRoles());
+        model.addAttribute("newUser", new User());
         return "admin-panel";
     }
 
 
     @PostMapping("/admin/save")
-    public String createNewUser(@ModelAttribute("user") User newUser
-    , @RequestParam(value = "selectedRolesNewUser", required = false) String role){
+    public String createNewUser(@ModelAttribute("newUser") User newUser){
         userService.saveUser(newUser);
         return "redirect:/admin";
     }
+
     @PostMapping("/admin/update")
     public String update(
-            @ModelAttribute("user") User user
-            ){
+            @ModelAttribute("user") User user){
         userService.updateUserById(user.getId(), user);
         return "redirect:/admin";
     }
 
 
     @PostMapping(value = "/admin/delete_user")
-    public String deleteUser (@RequestParam(value = "id") int id, Model model) {
+    public String deleteUser (@RequestParam(value = "id") int id) {
         userService.deleteUserById(id);
         return "redirect:/admin";
     }
 
 
 }
+
