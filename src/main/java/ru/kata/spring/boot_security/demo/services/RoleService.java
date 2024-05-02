5629bfde93644893f8b7f1a5a219518419a7e593
TopKop-svg <loookintome@gmail.com>
2024-05-03 02:48:32 +0900

    PP first with REST

--- src/main/java/ru/kata/spring/boot_security/demo/services/RoleService.java
+++ src/main/java/ru/kata/spring/boot_security/demo/services/RoleService.java
@@ -5,7 +5,5 @@ import ru.kata.spring.boot_security.demo.models.Role;
 import java.util.List;
 
 public interface RoleService {
-    List<Role> getAllRoles();
-    Role getRoleByName(String username);
 
 }
