796de0ffef1833c6fbfac126c452e5424be5b376
TopKop-svg <loookintome@gmail.com>
2024-05-09 21:41:11 +0900

    BaseController.java is finish

---
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/BaseController.java
@@ -0,0 +1,24 @@
+package ru.kata.spring.boot_security.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import ru.kata.spring.boot_security.demo.models.User;
+
+@Controller
+public class BaseController {
+    @GetMapping("/admin")
+    public String showAllUser(Model model) {
+        model.addAttribute("newUser", new User());
+        return "admin-panel-rest";
+    }
+    @GetMapping("/user")
+    public String showOneUser() {
+        return "user-panel-rest";
+    }
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login";
+    }
+}
